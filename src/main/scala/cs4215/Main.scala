c09fd4cbf0648166e98189201ab687b1361b4312
Paul Tan <pyokagan@gmail.com>
2019-03-31 22:45:55 +0800

    Add sample scene

--- src/main/scala/cs4215/Main.scala
+++ src/main/scala/cs4215/Main.scala
@@ -33,7 +33,21 @@ object Main {
     Scene += new Scene.ImageNode("bg uni.jpg") {
       pose.position.z = -10.0f
     }
-    Scene += new Scene.TextNode("Hello World!", 0f)
+    Scene += new Scene.ImageNode("sylvie blue normal.png") {
+      pose.position.y = -10.0f
+    }
+    Scene += new Scene.ImageNode("textbox.png") {
+      pose.position.z = 10.0f
+      pose.position.y = -268.0f
+    }
+    Scene += new Scene.TextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod diam nec elit ultrices rhoncus. Nullam non facilisis est. Morbi tempor facilisis aliquet. Donec eu feugiat sapien.") {
+      maxWidth = 700.0f / 0.3f
+      pose.position.z = 20.0f
+      pose.position.y = -220.0f
+      pose.position.x = -360.0f
+      pose.scale.x = 0.3f
+      pose.scale.y = 0.3f
+    }
   }
 
   private def loop(): Unit = withStack(stack => {

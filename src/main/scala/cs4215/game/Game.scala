e991ab45fdb9727f76d6ae8ee81468b16e7705cc
Yip Bing You <leafytubistfriend@gmail.com>
2019-04-05 14:11:45 +0800

    Implement char object

--- src/main/scala/cs4215/game/Game.scala
+++ src/main/scala/cs4215/game/Game.scala
@@ -10,7 +10,9 @@ import scala.concurrent.{ExecutionContext, Future, Promise}
  */
 object Game {
 
-  def say(char: String = "", msg: String)(implicit ec: ExecutionContext): Future[Unit] = {
+  class Character(val name: String = "", var color: String = "#ffffff")
+
+  def say(char: Character = new Character(), msg: String)(implicit ec: ExecutionContext): Future[Unit] = {
     val msgNode = new Scene.TextNode(msg)
     msgNode.maxWidth = 700.0f / 0.25f
     msgNode.pose.position.z = 20.0f
@@ -24,8 +26,8 @@ object Game {
     textbox.pose.position.z = 10.0f
     textbox.pose.position.y = -268.0f
 
-    //ToDo: Implement character customisation [Character Objects instead of Character String]
-    val charName = new Scene.TextNode(char)
+    //ToDo: Implement character color customisation
+    val charName = new Scene.TextNode(char.name)
     charName.maxWidth = 700.0f / 0.28f
     charName.pose.position.z = 20.0f
     charName.pose.position.y = -205.0f
@@ -49,13 +51,17 @@ object Game {
     } yield()
   }
 
+  def say(charName: String, msg: String)(implicit ec: ExecutionContext): Future[Unit] = {
+    say(new Character(charName), msg)
+  }
+
   def scene(texture: Texture)(implicit ec: ExecutionContext): Future[Unit] = {
     val bg = new Scene.ImageNode(texture)
     bg.pose.position.z = -100.0f
 
     for {
       _ <- {
-        Scene.clear
+        Scene.clear()
         Scene += bg
         Events.nextEvent
       }
@@ -67,7 +73,7 @@ object Game {
     val node = new Scene.SpriteNode(texture)
     for {
       _ <- {
-        Scene.clearSprites
+        Scene.clearSprites()
         Scene += node
         Events.nextEvent
       }
@@ -77,43 +83,8 @@ object Game {
 
 
   def run()(implicit ec: ExecutionContext): Future[Unit] = {
-    // TODO: Code to test Scene. To be removed.
-    /*
-    Scene += new Scene.ImageNode("bg uni.jpg") {
-      pose.position.z = -10.0f
-    }
-    Scene += new Scene.ImageNode("sylvie blue normal.png") {
-      pose.position.y = -10.0f
-    }
-    Scene += new Scene.ImageNode("sylvie blue smile.png") {
-      pose.position.z = 5.0f
-    }
 
-    Scene += new Scene.ImageNode("textbox.png") {
-      pose.position.z = 10.0f
-      pose.position.y = -268.0f
-    }
-
-
-        val node = new Scene.TextNode("")
-        node.maxWidth = 700.0f / 0.3f
-        node.pose.position.z = 20.0f
-        node.pose.position.y = -220.0f
-        node.pose.position.x = -360.0f
-        node.pose.scale.x = 0.3f
-        node.pose.scale.y = 0.3f
-        Scene += node
-
-
-        Scene += new Scene.TextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod diam nec elit ultrices rhoncus. Nullam non facilisis est. Morbi tempor facilisis aliquet. Donec eu feugiat sapien.") {
-          maxWidth = 700.0f / 0.3f
-          pose.position.z = 20.0f
-          pose.position.y = -220.0f
-          pose.position.x = -360.0f
-          pose.scale.x = 0.3f
-          pose.scale.y = 0.3f
-        }
-    */
+    val testChar = new Character("Sylvie", "#abcdef")
 
     for {
       _ <- scene("bg uni.jpg")
@@ -123,15 +94,13 @@ object Game {
       _ <- scene("bg meadow.jpg")
       _ <- say ("Test3", "In vino veritas")
       _ <- say ("Me4", "Hey... Umm...")
-
-
       _ <- show("sylvie blue giggle.png")
-      _ <- Events.waitForKeyPress(KeySpace)
+      _ <- say(testChar, msg = "She giggles.")
       _ <- show("sylvie green normal.png")
-      _ <- Events.waitForKeyPress(KeySpace)
+      _ <- say(testChar, msg = "She's in green.")
 
       _ <- scene("bg club.jpg")
-      _ <- say("Test5", "Ad Astra")
+      _ <- say("Test7", "Ad Astra")
 
       _ <- Events.waitForKeyPress(KeyEscape)
     } yield()
